498dcc598b492e11502f93d2dc4d138164ba33a7
Shanred <shanred28@gmail.com>
2023-06-02 08:39:41 +0300

    Работа со сценой

--- Assets/Scripts/Spawners/SpawnerVehicles.cs
+++ Assets/Scripts/Spawners/SpawnerVehicles.cs
@@ -8,7 +8,6 @@ namespace Hacaton
         [SerializeField] private Vehicles[] _prefabVechicles;
         [SerializeField] private Transform[] _spawnPlace;
 
-        [SerializeField] private float _timeRespawn;
         [SerializeField] private float _timeIntervalSpawn;
         [SerializeField] private int _maxRespawnVechicles;
         [SerializeField] private Transform[] _targetMove;
@@ -16,7 +15,6 @@ namespace Hacaton
         private List<Vehicles> _listVehicles;
 
 
-        private Timer _timeRespawnTimer;
         private Timer _timeIntervalSpawnTimer;
 
 
@@ -25,7 +23,6 @@ namespace Hacaton
         {
             InitTimers();
            _listVehicles = new List<Vehicles>();
-            SpawnVehicle();
         }
 
         private void Update()
@@ -41,12 +38,11 @@ namespace Hacaton
             int rndPrefVehicle = Random.Range(0, _prefabVechicles.Length);
             int rndTargetPoint = Random.Range(0, _spawnPlace.Length);
             Vector3 pos = _targetMove[rndTargetPoint].position - _spawnPlace[rndTargetPoint].position;
-            _prefabVechicles[rndPrefVehicle] = Instantiate(_prefabVechicles[rndPrefVehicle], _spawnPlace[rndTargetPoint].position, Quaternion.LookRotation(pos));
-           // _prefabVechicles[rndPrefVehicle] = Instantiate(_prefabVechicles[rndPrefVehicle], _spawnPlace[rndTargetPoint].position, Quaternion.Euler(_targetMove[rndTargetPoint].position));
-            Debug.Log(_prefabVechicles[rndPrefVehicle]);
+            Vehicles car = Instantiate(_prefabVechicles[rndPrefVehicle], _spawnPlace[rndTargetPoint].position, Quaternion.LookRotation(pos));          
+            Debug.Log(_listVehicles.Count);
 
-            _prefabVechicles[rndPrefVehicle].SetMoveTarget(_targetMove[rndTargetPoint]);
-            _prefabVechicles[rndPrefVehicle].SetPerent(this);
+            car.SetMoveTarget(_targetMove[rndTargetPoint]);
+            car.SetPerent(this);
             _timeIntervalSpawnTimer.Restart();
                  
         }
@@ -62,13 +58,11 @@ namespace Hacaton
 
         private void InitTimers()
         {
-            _timeRespawnTimer = new Timer(_timeRespawn);
             _timeIntervalSpawnTimer = new Timer(_timeIntervalSpawn);
         }
 
         private void UpdateTimers()
         {
-            _timeRespawnTimer.RemoveTime(Time.deltaTime);
             _timeIntervalSpawnTimer.RemoveTime(Time.deltaTime);
         }
     
